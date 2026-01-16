4d1dcd6f6ce04c1b9338ee9a1378a3c813f3c927
ApplianceRepairServicesNYC <fabianrivera2020@gmail.com>
2026-01-16 11:57:35 -0500

    a

--- backend/src/technicians/dto/lock-technician.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LockTechnicianDto {
-  @ApiProperty({ example: 'Failed to meet weekly quota' })
-  @IsString()
-  @MinLength(5)
-  reason: string;
-}
